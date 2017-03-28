c66fae8ba169244136d64984c080eb2e6767caef
Jason Curry <kurrikov@gmail.com>
2017-03-28 18:41:13 -0500

    Bidirectional serial work

--- Rover_Code/Rover_Code.ino
+++ Rover_Code/Rover_Code.ino
@@ -7,8 +7,7 @@
 
 #define SERIAL_DEBUG 1
 // Pin definitions
-#define SPRAYCAN_PIN 9
-#define TRIGGER_BTN_PIN 11
+#define SPRAYCAN_PIN 7
 #define ZERO_BTN_OUTPUT 13
 #define ARRAY_LENGTH 2
 
@@ -26,13 +25,14 @@ float deadZoneRange = 0.06;
 
 Servo spraycan;
 int zero_value = 0;
+int sensitivity; // TODO: default value for sensitivity?
+int found = 0;
 SoftwareSerial xbee(10, 11); // RX, TX
+SoftwareSerial detector(8, 9); // RX, TX
 
 void setup() {
-  pinMode(MARKING_PIN, OUTPUT);
-  pinMode(TRIGGER_BTN_PIN, INPUT_PULLUP);
-  pinMode(RESET_BTN_PIN, INPUT_PULLUP);
-  pinMode(RESET_BTN_OUTPUT, OUTPUT);
+  pinMode(SPRAYCAN_PIN, OUTPUT);
+  pinMode(ZERO_BTN_OUTPUT, OUTPUT);
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   right_motor->run(RELEASE);
@@ -46,20 +46,25 @@ void setup() {
   Serial.println("Serial connected");
   #endif
 
-  // Set the data rate for the SoftwareSerial port
+  // Set the data rate for the SoftwareSerial ports
   xbee.begin(9600);
+  //detector.begin(9600);
   #if SERIAL_DEBUG
-  Serial.println("Xbee connected");
+  Serial.println("Software serial ports connected");
   #endif
 }
 
 
 void loop() {
   if (xbee.available()) {
-    // read from serial and parse incoming data
+    // Read from serial and parse incoming data
+    // TODO: Add synchronization
     motorValues[0] = xbee.parseInt();
     motorValues[1] = xbee.parseInt();
     zero_value = xbee.parseInt();
+    sensitivity = xbee.parseInt();
+
+    // Initialize zero position and deadzone for motor control
     if (firstTime) {
       initialMotorValues[0] = motorValues[0];
       initialMotorValues[1] = motorValues[1];
@@ -70,15 +75,25 @@ void loop() {
 
     // Debugging
     #if SERIAL_DEBUG
-    Serial.println(motorValues[0] + ", " + motorValues[1] + ", " + motorValues[2]);
+    Serial.println(String(motorValues[0]) + ", " + motorValues[1] + ", " + motorValues[2]);
     Serial.print("Calulated Offsets: ");
     Serial.println(String(deadZoneOffset[0]) + ", " + deadZoneOffset[1] + ", " + deadZoneOffset[2]);
     #endif
 
     // Adjust the speed and direction of the right and left motors
     controlMotor();
-    // Pass the value of the zero button, which is itself read xbee, to the metal detector Uno
-    // digitalWrite(ZERO_BTN_OUTPUT, zero_value);
+  }
+
+  // Send sensitivity and zeroing instructions to the metal detector Uno
+  char buffer[20];
+  sprintf(buffer, "%d,%d", sensitivity, zero_value);
+  detector.println(buffer);
+
+  // Read from metal detector Uno
+  if (detector.available()) {
+    found = detector.parseInt();
+    sprintf(buffer, "%d", found);
+    xbee.println(buffer);
   }
 }
 
