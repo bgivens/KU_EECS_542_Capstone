7b34793ba467674e26684eae0f9d32c7f5c0f801
dnorman2323 <dnorman2323@gmail.com>
2017-04-20 14:10:15 -0500

    Servo Code

---
+++ servo_test.ino
@@ -0,0 +1,20 @@
+
+#include <Servo.h>
+
+
+Servo spraycan;
+
+void setup() {
+
+  spraycan.attach(9);
+  spraycan.write(180);
+  delay(400);
+  spraycan.write(100);  
+  delay(250);
+  spraycan.write(180);
+}
+
+void loop() {
+  
+}
+
